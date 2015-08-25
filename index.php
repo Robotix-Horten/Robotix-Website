7a46ab82f8a03698f587e186c1ff663ce604cd0c
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2015-08-25 18:09:43 +0200

    Added news

--- index.php
+++ index.php
@@ -11,6 +11,16 @@
   		<li class="active"><a href="#">Home</a></li>
 	</ol>
 
+	<div class="container">
+		<h1>Nyheter</h1>
+		<div>
+			<h3>25.08.2015</h3>
+			<p>Da er det klart for start etter sommerferien igjen! Nå må vi stå på for å vinne FLL folkens!</p>
+			<p>Sjekk Programmet</p>
+		</div>
+	</div>
+
+
 	<script type="text/javascript" src="./bower_components/jquery/dist/jquery.min.js"></script>
 	<script type="text/javascript" src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
