34c26fb46b0dc42a21ca4fbd829ccaf0e19a0790
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2017-01-29 17:11:47 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+	require("header.php");
+	require("menu.php");
+?>
+
+<div class="container-fluid" id="content">
+	<div id="news" class="col-md-7">
+		<h1>Om Oss</h1>
+
+		<p>Vi er en klubb med omlag 15 medlemmer og vi holder til i Sentrumskrika i Horten. Vi bygger og programmerer Lego-roboter, har LAN-party og drar på turer sammen. Hvert år melder vi oss på FLL (First Lego League) som er en verdensomspennende konkuranse i Lego-roboter. Her blir mange sider av laget vurdert og det laget som har det beste samlede inntrykket gør videre med tittelen "FLL Champions". Dette foregår om høsten, og starter med små distriktsfinaler som sender sine "champions" videre til den Nordiske finalen. Vi har vårt med i den nordiske finalen tre ganger.</p>
+		<p>For mere info se denne linken om FLL</p>
+		<p>Ellers så arrangerer vi LAN-party hvor alle medlemmene tar med seg PCen sin inn i kirka og sitter og spiller dataspill hele natta sammen, dette er alltid en stor suksess! Vi har også pleid å dra til Knatholmen leirsted både for å ha det hyggelig sammen en helg, men også for å virkelig kunne sette seg inn i Lego-programmeringen.</p>
+	</div>
+
+	<div id="showcase" class="col-md-4">
+		<h1>Showcase</h1>
+		<img class="img-responsive" src="resources/pokalene_09.jpg">
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,44 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Robotix - Hjem</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<?php 
+	require("header.php");
+	require("menu.php");
+?>
 
-	<link rel="stylesheet" type="text/css" href="menu.css">
-	<link rel="stylesheet" type="text/css" href="main.css">
-
-</head>
-<body>
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<nav class="navbar navbar-default">
-	<div class="container">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="/"><img src="logo.gif"></a>
-		</div>
-
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="navbar">
-		<ul class="nav navbar-nav">
-			<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-			<li><a href="#">About</a></li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="#">Login</a></li>
-		</ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
-</nav>
-
-<div class="container" id="content">
+<div class="container-fluid" id="content">
 	<div id="news" class="col-md-7">
 		<h1>Nyheter</h1>
 
@@ -50,7 +15,7 @@
 
 	<div id="showcase" class="col-md-4">
 		<h1>Showcase</h1>
-		<img src="action.png">
+		<img class="img-responsive" src="resources/action.png">
 	</div>
 </div>
 
