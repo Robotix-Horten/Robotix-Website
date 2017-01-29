d6c2937399eaf61a00c9d9210c1956a0d5a1f8c3
Daniel Løvbrøtte Olsen <daniel.olsen99+GitHub@gmail.com>
2017-01-29 01:56:47 +0100

    4th now? I think

--- index.php
+++ index.php
@@ -1,25 +1,59 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Robotix - Hjem</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-	<link rel="stylesheet" type="text/css" href="./bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="menu.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+
 </head>
 <body>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-	<?php include "./Resources/php/menu.php"; ?>
-
+<nav class="navbar navbar-default">
 	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/"><img src="logo.gif"></a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="navbar">
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+			<li><a href="#">About</a></li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Login</a></li>
+		</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
+
+<div class="container" id="content">
+	<div id="news" class="col-md-7">
 		<h1>Nyheter</h1>
-		<div>
-			<h3>25.08.2015</h3>
-			<p>Da er det klart for start etter sommerferien igjen! Nå må vi stå på for å vinne FLL folkens!</p>
-			<p>Sjekk Programmet</p>
+
+		<div class="news-item">
+			<h3><span class="timestamp">29. Januar</span> Ny nettside!</h3>
+			<p>Nettsiden har trengt en opprydning lenge nå, og det er endelig gjort! Gled dere til mange nye features i fremtiden!</p>
 		</div>
 	</div>
 
+	<div id="showcase" class="col-md-4">
+		<h1>Showcase</h1>
+		<img src="action.png">
+	</div>
+</div>
 
-	<script type="text/javascript" src="./bower_components/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
