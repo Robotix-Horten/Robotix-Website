4f036397790fc7e3f00ea590924e4bd941338c14
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2017-01-30 21:15:23 +0100

    Closes #2

--- program.php
+++ program.php
@@ -4,9 +4,76 @@
 ?>
 
 <div class="container" id="content">
-	<div id="news" class="col-md-7">
-		
+	<div id="table" class="col-md-7">
+		<h1>Program</h1>
+		<table class="table">
+			<tbody><tr>
+				<th>Dato</th>
+				<th>Tema</th>
+			</tr>
+			<tr>
+				<td>05. Januar</td>
+				<td>*Infomøte m/foreldre*</td>
+			</tr>
+			<tr>
+				<td>19. Januar</td>
+				<td>Oppgave/td&gt;
+			</td></tr>
+			<tr>
+				<td>02. Februar</td>
+				<td>Konkurranse</td>
+			</tr>
+			<tr>
+				<td>16. Februar</td>
+				<td>Oppgave</td>
+			</tr>
+			<tr>
+				<td>19 - 20. Februar</td>
+				<td>*LAN*</td>
+			</tr>
+			<tr>
+				<td>01. Mars</td>
+				<td>Konkurranse</td>
+			</tr>
+			<tr>
+				<td>08. Mars</td>
+				<td>Oppgave</td>
+			</tr>
+			<tr>
+				<td>29. Mars</td>
+				<td>Konkurranse</td>
+			</tr>
+			<tr>
+				<td>12. April</td>
+				<td>Oppgave</td>
+			</tr>
+			<tr>
+				<td>26. April</td>
+				<td>Konkurranse</td>
+			</tr>
+			<tr>
+				<td>10. Mai</td>
+				<td>Oppgave</td>
+			</tr>
+			<tr>
+				<td>24. Mai</td>
+				<td>Konkurranse - (Oppgave?)</td>
+			</tr>
+			<tr>
+				<td>07. Juni</td>
+				<td>*Avslutning m/foreldre (konkurranse?)*</td>
+			</tr></tbody>
+		</table>
 	</div>
 
-	<div id="showcase" class="col-md-4"></div>
+	<div id="showcase" class="col-md-4">
+		<h1>Informasjon</h1>
+		<ul>		
+			<li>Klubbkveldene våre har vi i Sentrumskirken, Thoresens gate 3, Horten</li>
+			<li>Vi møtes ca annenhver tirsdag, starter 18:30 (presis!) og vi slutter 20:30.</li>
+			<li>Av og til har vi noe ekstra på planene, men da står det alltid spesiell beskjed i programmet.</li>
+			<li>Datoer med * er spesialprogram - mere beskjed kommer.</li>
+
+		</ul>
+	</div>
 </div>
