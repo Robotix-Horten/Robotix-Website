46eea1b99c3b6edc18bc5c4767914031c826da55
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2017-02-25 16:44:45 +0100

    added nito as a sponsor, also centered the sponsors and cleaned up a bit

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <footer class="footer">
 	<div class="container">
-		<img class="img-responsive sponsor" src="./resources/kongsberg.png">
-		<img class="img-responsive sponsor" src="./resources/jotron.png">
+		<div class="row">
+			<div class="col-md-2"><img id="footer_kongsberg" class="center-block img-responsive sponsor" src="./resources/logos/kongsberg.png"></div>
+			<div class="col-md-4"><img id="footer_nito" class="center-block img-responsive sponsor" src="./resources/logos/nito-logo-gronn.png"></div>
+			<div class="col-md-4"><img id="footer_jotron" class="center-block img-responsive sponsor" src="./resources/logos/jotron.png"></div>
+		</div>	
 	</div>
 </footer>
 
