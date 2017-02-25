9ac582869c498cd8d6a23d3a9b7989d61a43caec
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2017-02-25 20:39:56 +0100

    add sparebankstiftelsen

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
 			<div class="col-md-2"><img id="footer_kongsberg" class="center-block img-responsive sponsor" src="./resources/logos/kongsberg.png"></div>
 			<div class="col-md-4"><img id="footer_nito" class="center-block img-responsive sponsor" src="./resources/logos/nito-logo-gronn.png"></div>
 			<div class="col-md-4"><img id="footer_jotron" class="center-block img-responsive sponsor" src="./resources/logos/jotron.png"></div>
+			<div class"col-md-2"><img id="footer_sparebankstiftelsen" class="center-block img-responsive sponsor" src="./resources/logos/sbs-logo-negative.png"></div></div>
 		</div>	
 	</div>
 </footer>
